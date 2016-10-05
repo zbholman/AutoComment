9654d6a59a4367fbc26a2b7bf0155436952b9085
Brian Holman <zbholman@gmail.com>
2016-10-05 18:19:25 -0400

    added code for java comments

--- auto_comment.sh
+++ auto_comment.sh
@@ -19,46 +19,46 @@ read fileType
 
 todaysDate=$(date +%m-%d-%Y) # Date format of MM-DD-YYYY
 
+case $fileType in
+	'py')
+		commentChar='#'
+		;;
+	'java')
+		commentChar='\/\/'
+		;;
+esac
+
 ### Create comment format
-headerComment="# Author: $firstName $lastName&\n# Project: $projectName&\n# Date: $todaysDate&\n&\n"
+headerComment="$commentChar Author: $firstName $lastName&\n$commentChar Project: $projectName&\n$commentChar Date: $todaysDate&\n&\n"
 
 ### Function for adding comment if it doesn't exist
 function checkHeader() {
 
 	# Variable to check the first line of the file
 	firstChar=$(head -c 1 $1)
+	firstCommentChar=$(echo -n $2 | tail -c 1)
 
-	# If the first line starts with '# Author' skip file
-	if [[ "$firstLine" != "#" ]]; then
+	# If the first line starts with a comment character skip file
+	if [[ "$firstChar" != "$firstCommentChar" ]]; then
 
 		# If the first line doesn't, ask user if they created it
 		printf "\n$f doesn't have a header comment\n"
 		read -p "If you created it, would you like to add a header comment? (y/n)" -n 1 -r
-		echo
 
 		# If they reply 'y', then create header comment
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			
-			# If the user is on mac, use gsed command (requires homebrew and gsed)
-			if [ "$(uname)" == "Darwin" ]; then
-				# Enter header comment into the top of the file
-    			gsed -i "1s/^/$headerComment/" $1
-	
-    		# If the user is on linux, used normal sed command
-			elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 				sed -i "1s/^/$headerComment/" $1
-			fi
 		fi
+		echo
 	fi
 }
-
-### Navigate to team directory
-cd $teamDirectory
 
 ### Set internal file seperator to recognize spaces in filenames
 IFS=$'\n'
 
 ### Find all python files in team directory, and run header function
-for f in $(find . -name ); do
-  checkHeader $f
+for f in $(find . -name \*.$fileType); do
+  checkHeader $f $commentChar
 done
+
+printf "\nEnd of Results\n\n"
