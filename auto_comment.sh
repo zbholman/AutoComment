65978e0a034b130b423d4992c9bd82e043f5ed8e
zbholman <zbholman@gmail.com>
2016-10-05 16:47:14 -0400

    Made script more generic

--- auto_comment.sh
+++ auto_comment.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # This script creates the comment header for python files you've created
 # Author: Brian Holman
-# Course: IST 440
 # Date: 10/02/2016
 
 ### Description
 printf "\nThis script creates comment headers for your python files\n"
 printf "Hopefully it saves you some time\n\n"
 
-### Create variables
+### Gather input for variables
 printf "Enter your first name:\n"
 read firstName
 printf "\nEnter your last name:\n"
 read lastName
-printf "\nEnter your team directory in the git repo, ex: LightingSystem\n"
-read teamDirectory
+printf "\nWhat project is this for?\n"
+read projectName
+printf "\nWhat file extension are you looking for? ex: py\n"
+read fileType
+
 todaysDate=$(date +%m-%d-%Y) # Date format of MM-DD-YYYY
 
 ### Create comment format
-headerComment="# Author: $firstName $lastName&\n# Course: IST 440&\n# Team: $teamDirectory&\n# Date: $todaysDate&\n&\n"
+headerComment="# Author: $firstName $lastName&\n# Project: $projectName&\n# Date: $todaysDate&\n&\n"
 
 ### Function for adding comment if it doesn't exist
 function checkHeader() {
@@ -57,6 +59,6 @@ cd $teamDirectory
 IFS=$'\n'
 
 ### Find all python files in team directory, and run header function
-for f in $(find . -name '*.py'); do
+for f in $(find . -name ); do
   checkHeader $f
 done
